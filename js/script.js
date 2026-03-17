bc086bf8e191052e0beaec17a00171cd79b4e141
eaa26natp-debug <eaa26natp@students.eaaa.dk>
2026-03-17 13:04:43 +0100

    selektoren #bubble1  er opdatet

--- js/script.js
+++ js/script.js
@@ -2,25 +2,37 @@
 
 const audio = document.getElementById("bg-sound");
 audio.play();
-document.removeEventListener("click", startSound);
+//document.removeEventListener("click", startSound);
+
+
 
 const bubble = document.getElementById("bubble1");
 const sound = new Audio("sound/bubble-pop.mp3");
 console.log(bubble);
 
 bubble.addEventListener("click", () => {
-  sound.currentTime = 0;
-  sound.play();
+    sound.currentTime = 0;
+    sound.play();
+    console.log("test")
 
-  bubble.style.transform = "scale(1.3)";
-  bubble.style.opacity = "0";
-  console.log("test");
+    bubble.style.transform = "scale(1.3)";
+    bubble.style.opacity = "0";
+    console.log("test")
 
-  setTimeout(() => {
-    bubble.style.transform = "scale(1)";
-    bubble.style.opacity = "1";
-  }, 300);
+    setTimeout( () => {
+         bubble.style.transform = "scale(1)";
+        bubble.style.opacity = "1";
+    }, 300);
 });
+
+let createbubble = document.createElement("img");
+createbubble.classList.add("bubble")
+    createbubble.src = "imges/bubble.png"
+    document.body.appendChild(createbubble);
+    setTimeout(() => {
+    bubble.style.bottom = "0px";
+    bubble.style.opacity = "1";
+}, 300);
 
 // bubble.addEventListener("clik", () => {
 //     bubble.style.transform = "scale(1.3)";
