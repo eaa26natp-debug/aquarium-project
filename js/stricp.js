1e13aa54634d3df994d432fb85a5f594d5214624
Mettevebert <mettevebert@gmail.com>
2026-03-17 11:01:32 +0100

    fix
    Changed background sound to constantly play instead of on click

--- js/stricp.js
+++ js/stricp.js
@@ -1,22 +1,26 @@
 "use strict";
 
+const audio = document.getElementById("bg-sound");
+audio.play();
+document.removeEventListener("click", startSound);
+
 const bubble = document.getElementById("bubble1");
-const sound = new Audio ("sound/bubble-pop.mp3");
+const sound = new Audio("sound/bubble-pop.mp3");
 console.log(bubble);
 
 bubble.addEventListener("click", () => {
-    sound.currentTime = 0;
-    sound.play();
+  sound.currentTime = 0;
+  sound.play();
 
-    bubble.style.transform = "scale(1.3)";
-    bubble.style.opacity = "0";
-    console.log("test")
+  bubble.style.transform = "scale(1.3)";
+  bubble.style.opacity = "0";
+  console.log("test");
 
-    setTimeout( () => {
-         bubble.style.transform = "scale(1)";
-        bubble.style.opacity = "1";
-    }, 300);
-})
+  setTimeout(() => {
+    bubble.style.transform = "scale(1)";
+    bubble.style.opacity = "1";
+  }, 300);
+});
 
 // bubble.addEventListener("clik", () => {
 //     bubble.style.transform = "scale(1.3)";
@@ -28,8 +32,3 @@ bubble.addEventListener("click", () => {
 //     }, 200);
 // });
 
-document.addEventListener('click', function startSound() {
-  const audio = document.getElementById('bg-sound');
-  audio.play();
-  document.removeEventListener('click', startSound);
-});
