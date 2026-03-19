6dfecebcc54904b0dc091683ad0f79844e6f8586
Boglarka1212 <kazmer.boglarka@gmail.com>
2026-03-19 14:56:30 +0100

    feat: tilbageknap funktionel

--- js/sektioner.js
+++ js/sektioner.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /* toggle popup visibility */
-const goBackList = Array.from(document.getElementsByClassName("go-back"));
+/* const goBackList = Array.from(document.getElementsByClassName("go-back"));
 
 goBackList.forEach((goBackBtn) => {
   goBackBtn.addEventListener("click", () => {
@@ -10,7 +10,22 @@ goBackList.forEach((goBackBtn) => {
 
     document.getElementById("fiske-popup").classList.add("popup-synlig"); 
   });
-});
+}); */
+
+function goBack(from) {
+if (from == "habitat") {
+  document.getElementById("habitat-popup").classList.remove("popup-synlig");
+}
+if (from == "food") {
+  document.getElementById("food-popup").classList.remove("popup-synlig");
+}
+if (from == "readmore") {
+  document.getElementById("readmore-popup").classList.remove("popup-synlig");
+}
+
+  document.getElementById("fiske-popup").classList.remove("popup-hidden");
+  document.getElementById("fiske-popup").classList.add("popup-synlig");
+}
 
 /* Info redirect to food popup */
 
@@ -44,11 +59,9 @@ function openHabitatPopup() {
   if (!thisFish) return;
 
   document.getElementById("habitat-navn").textContent = thisFish.name;
-  document.getElementById("habitat-latin").textContent =
-    thisFish["latinskName"];
+  document.getElementById("habitat-latin").textContent = thisFish["latinskName"];
 
-  document.getElementById("habitat-img").src =
-    "imges/habitat-globus/" + thisFish["habitat-img"];
+  document.getElementById("habitat-img").src = "imges/habitat-globus/" + thisFish["habitat-img"];
   document.getElementById("habitat-text").textContent = thisFish["habitatText"];
 
   document.getElementById("fiske-popup").classList.remove("popup-synlig");
